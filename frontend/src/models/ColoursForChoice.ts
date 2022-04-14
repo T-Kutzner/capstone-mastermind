9518176f1da182d49af6d5604e8e6de8203a6b4e
Tanja Kutzner <t.kutzner@arcor.de>
2022-04-14 12:06:02 +0200

    Added colours to choice from

---
+++ frontend/src/models/ColoursForChoice.ts
@@ -0,0 +1,10 @@
+export const ColoursForChoice:{[key: number]: any} = {
+    0: '#741B47',
+    1: '#134F5C',
+    2: '#E69138',
+    3: '#4da63c',
+    4: '#FFD966',
+    5: '#9900FF',
+    6: '#777777',
+    7: '#76A5AF'
+}
